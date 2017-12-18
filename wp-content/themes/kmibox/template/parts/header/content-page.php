3ee3778e16bfa0e102ca433724b5fac9e37a2c12
kharlos <32462475+yookharlos@users.noreply.github.com>
2017-12-18 11:42:14 -0400

    NIVEL 1 - LANDING (RESPONSIVE)
    Los botones de iniciar sesión y registro, no contienen descripción.

--- wp-content/themes/kmibox/template/parts/header/content-page.php
+++ wp-content/themes/kmibox/template/parts/header/content-page.php
@@ -4,10 +4,10 @@
  		<aside id="top"> 
 			<div class="container">
 				<a href="<?php echo get_home_url(); ?>">
-					<img src="<?php echo get_home_url(); ?>/img/marca-logo.png"  >
+					<img src="<?php echo get_home_url(); ?>/img/logo-text-white.png"  >
 				</a>
 				
-			<ul class="option-menu list-unstyled list-inline col-xs-6 col-md-6 pull-right text-right">
+			<ul class="option-menu list-unstyled list-inline col-xs-8 col-md-6 pull-right text-right">
 					<?php if ( is_user_logged_in() ){ ?>
 						<li>
 							<a class="btn-kmibox-link" href="<?php echo get_home_url(); ?>/perfil-usuario">
@@ -23,12 +23,12 @@
 					<?php }else{ ?>
 						<li>
 							<a class="btn-kmibox-link" href="<?php echo get_home_url(); ?>/iniciar-sesion">
-							<i class="fa fa-key" aria-hidden="true"></i>
+							<i class="fa fa-key fa-lg" aria-hidden="true"></i>
 							<span >Iniciar Sesion</span></a><!--href="<?php echo get_home_url(); ?>/iniciar-sesion">-->
 						</li>
 						<li>
 							<a class="btn-kmibox-link" href="<?php echo get_home_url(); ?>/registro">
-							<i class="fa fa-user-plus" aria-hidden="true"></i> <span >Registro</span></a><!--href="<?php echo get_home_url(); ?>/registro">-->
+							<i class="fa fa-user-plus fa-lg" aria-hidden="true"></i> <span >Registro</span></a><!--href="<?php echo get_home_url(); ?>/registro">-->
 						</li>
 					<?php } ?>
 
