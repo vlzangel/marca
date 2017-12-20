75b5968e9a42ac78eb2e20276ce0f1e79a869c28
kharlos <32462475+yookharlos@users.noreply.github.com>
2017-12-20 18:51:53 -0400

    arreglos

--- wp-content/themes/kmibox/quiero-mi-marca.php
+++ wp-content/themes/kmibox/quiero-mi-marca.php
@@ -122,25 +122,25 @@
 					<div id="planes">
 						<article id="plan-Quincenal" class="text-center col-sm-4 separation-top col-xs-6">
 							<img class="img-responsive" src="'.get_home_url().'/img/Quincenal.png" width="300px" height="370px">
-							<button class="btn btn-sm-kmibox btn-sm-kmibox-price caviar" data-value="Quincenal" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777;" > Quincenal </button>
+							<button class="btn btn-sm-marca btn-sm-kmibox-price caviar" data-value="Quincenal" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777;" > Quincenal </button>
 						</article>
 						<article id="plan-Mensual" class="text-center col-sm-4 separation-top col-xs-6">
 							<img class="img-responsive" src="'.get_home_url().'/img/Mensual.png" width="300px" height="370px">
-							<button class="btn btn-sm-kmibox btn-sm-kmibox-price caviar" data-value="Mensual" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777;">Mensual</button>
+							<button class="btn btn-sm-marca btn-sm-kmibox-price caviar" data-value="Mensual" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777;">Mensual</button>
 						</article>
 						<!-- <article id="plan-Bimestral" class="text-center col-sm-4 separation-top col-xs-offset-3 col-xs-6 arriba"> -->
 
 						<article id="plan-Bimestral" class="text-center col-sm-4 separation-top col-xs-6 hidden-sm hidden-xs" style="display: inline-block;/*margin-left: 26%; margin-top: 7%;*/">
 							<img class="img-responsive" src="'.get_home_url().'/img/Bimestral.png" width="300px" height="370px">
-							<button class="btn btn-sm-kmibox btn-sm-kmibox-price caviar" data-value="Bimestral" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777; margin-top: 0px">Bimestral</button>
+							<button class="btn btn-sm-marca btn-sm-kmibox-price caviar" data-value="Bimestral" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777; margin-top: 0px">Bimestral</button>
 						</article>
 
 						<article id="plan-Bimestral" class="text-center col-sm-4 separation-top col-xs-6 hidden-xs hidden-lg hidden-md visible-sm" style="display: inline-block;margin-top: 7%;">
 							<img class="img-responsive" src="'.get_home_url().'/img/Bimestral.png" width="300px" height="370px">
-							<button class="btn btn-sm-kmibox btn-sm-kmibox-price caviar" data-value="Bimestral" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777;">Bimestral</button>
+							<button class="btn btn-sm-marca btn-sm-kmibox-price caviar" data-value="Bimestral" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777;">Bimestral</button>
 						</article>
 						<article id="plan-Bimestral" class="text-center col-sm-4 separation-top col-xs-6 hidden-sm hidden-lg hidden-md visible-xs" style="display: inline-block;margin-left:  26%;margin-top: 0%;">	<img class="img-responsive" src="'.get_home_url().'/img/Bimestral.png" width="300px" height="370px">
-							<button class="btn btn-sm-kmibox btn-sm-kmibox-price caviar" data-value="Bimestral" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777;">Bimestral</button>
+							<button class="btn btn-sm-marca btn-sm-kmibox-price caviar" data-value="Bimestral" style="border: solid 2px rgb(144, 14, 156); background: #ffffff; color: #777;">Bimestral</button>
 						</article>
 
 					</div>
