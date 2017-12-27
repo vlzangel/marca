d74fce8365d10a923e7f2dad486ad8d2cebe7033
kharlos <32462475+yookharlos@users.noreply.github.com>
2017-12-27 13:17:58 -0400

    tamaño de la imagen y texto centrado

--- wp-content/themes/kmibox/template/parts/footer/footer-page.php
+++ wp-content/themes/kmibox/template/parts/footer/footer-page.php
@@ -12,7 +12,8 @@
 				<li class="caviar">Preguntas y respuestas</li>
 			</ul>
 		</article>
-		<img src="<?php echo get_home_url(); ?>/img/Image-footer.png" class="img-responsive" width="50%" style="margin-top: 34%">
+		<img src="<?php echo get_home_url(); ?>/img/Image-footer.png" class="hidden-sm hidden-ms hidden-lg" width="85%" style="margin-top: 34%">
+		<img src="<?php echo get_home_url(); ?>/img/Image-footer.png"  width="50%" style="margin-top: -46%; margin-left: 55%;">
 	</div>		 	
 	<div style="float:right;width:50%;"> 
 		<article class="col-sm-12  text-left">
