9d7435a95fb462f506c3660288c624e1c3fe7313
kharlos <32462475+yookharlos@users.noreply.github.com>
2018-01-04 13:50:43 -0400

    observaciones

--- wp-content/themes/kmibox/template/parts/page/login-page.php
+++ wp-content/themes/kmibox/template/parts/page/login-page.php
@@ -2,8 +2,8 @@
 <aside class="col-md-6 col-xs-12 hidden col-md-offset-3 alert alert-danger" id="login-mensaje"></aside>
 <div class="col-md-6"><img src="<?php echo get_home_url(); ?>/img/Logo.png" class="img-responsive" style="margin-top: 20%;margin-left: 7%;width: 78%;"></div>
 <article id="inicio-sesion" 
-		class="col-md-6 col-xs-12 col-md-offset-10" 
-		style="background-color: #000000;height: 600px;margin-top: -26%;">
+		class="col-md-6 col-xs-12 col-md-offset-6" 
+		style="background-color: #000000;height: 636px;margin-top: -31%;">
 
 	
 		<div class="col-md-10 col-md-offset-1 form-horizontal">
