a3711e3c0354a883d1748881cd0333fe7e044833
kharlos <32462475+yookharlos@users.noreply.github.com>
2018-01-05 11:47:50 -0400

    tamaño de letra, margen y color
    tamaño de letra, margen y color del perfil luego de estar logueado

--- wp-content/themes/kmibox/template/info.php
+++ wp-content/themes/kmibox/template/info.php
@@ -9,7 +9,7 @@
 
 						<div class="col-sm-12">
 							<div class="row">
-								<h2 class="col-sm-6">Información del usuario</h2>
+								<h2 class="col-sm-6 gothan">Información del usuario</h2>
 							</div>
 						</div>
 
@@ -84,7 +84,7 @@
 						</div>
 
 							<div class="row">
-								<h2 class="col-sm-6">Información de cuenta</h2>
+								<h2 class="col-sm-6 gothan" style="margin-left: 3%; font-size: 18px">Información de cuenta</h2>
 							</div>
 
 						<div class="col-md-12">
@@ -96,7 +96,7 @@
 						 			
 						<div class="col-sm-12 ">
 							<div class="row">
-								<h2 class="col-sm-6">Dirección</h2>
+								<h2 class="col-sm-6 gothan" style="margin-left: 1%; font-size: 18px">Dirección</h2>
 							</div>
 						</div>
 
