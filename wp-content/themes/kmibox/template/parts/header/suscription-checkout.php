e7961a3e23854b0666b6c40a2547ca29dc551702
kharlos <32462475+yookharlos@users.noreply.github.com>
2018-01-08 15:08:45 -0400

    observaciones

--- wp-content/themes/kmibox/template/parts/header/suscription-checkout.php
+++ wp-content/themes/kmibox/template/parts/header/suscription-checkout.php
@@ -18,19 +18,19 @@
 
 						<a href="<?php echo get_home_url(); ?>/quiero-mi-marca"  class="btn-kmibox-link-suscription">
 						<i class="fa fa-plus-circle" aria-hidden="true"></i>
-						<span class="hidden-xs gothan ">Quiero mi NutriHeroes</span></a>
+						<span class="hidden-xs hidden-sm gothan ">Quiero mi NutriHeroes</span></a>
 						</li>
 
 						<li>
 							<a class="btn-kmibox-link-suscription" href="<?php echo get_home_url() ?>/?source=<?php echo get_source_url(); ?>">
 							<i class="fa fa-user" aria-hidden="true"></i>
-							<span class="hidden-xs gothan "><?php echo $user->display_name; ?></span></a>
+							<span class="hidden-xs  hidden-sm  gothan "><?php echo $user->display_name; ?></span></a>
 						</li> 
 						<li>
 							<a class="btn-kmibox-link-suscription" 
 							href="<?php echo wp_logout_url( get_home_url() );?> " style="    margin-right: -60%;">
 							<i class="fa fa-close" aria-hidden="true"></i>
-							<span class="hidden-xs gothan ">Salir</span></a>
+							<span class="hidden-xs  hidden-sm gothan ">Salir</span></a>
 						</li>
 					<?php }else{ ?>
 
@@ -39,7 +39,7 @@
 						<li>
 							<a class="btn-kmibox-link-suscription" href="<?php echo get_home_url(); ?>/iniciar-sesion">
 							<i class="fa fa-key" aria-hidden="true"></i>
-							<span class="hidden-xs gothan ">Iniciar Sesion</span></a>
+							<span class="hidden-xs  gothan ">Iniciar Sesion</span></a>
 						</li>
 						<li>
 							<a class="btn-kmibox-link-suscription" href="<?php echo get_home_url(); ?>/registro">
@@ -56,19 +56,19 @@
 
 						<a href="<?php echo get_home_url(); ?>/quiero-mi-marca"  class="btn-kmibox-link-suscription">
 						<i class="fa fa-plus-circle" aria-hidden="true"></i>
-						<span class="hidden-xs gothan ">Quiero mi NutriHeroes</span></a>
+						<span class="hidden-xs hidden-sm gothan ">Quiero mi NutriHeroes</span></a>
 						</li>
 
 						<li>
 							<a class="btn-kmibox-link-suscription" href="<?php echo get_home_url() ?>/?source=<?php echo get_source_url(); ?>">
 							<i class="fa fa-user" aria-hidden="true"></i>
-							<span class="hidden-xs gothan "><?php echo $user->display_name; ?></span></a>
+							<span class="hidden-xs hidden-sm gothan "><?php echo $user->display_name; ?></span></a>
 						</li> 
 						<li>
 							<a class="btn-kmibox-link-suscription" 
 							href="<?php echo wp_logout_url( get_home_url() );?> ">
 							<i class="fa fa-close" aria-hidden="true"></i>
-							<span class="hidden-xs gothan ">Salir</span></a>
+							<span class="hidden-xs hidden-sm gothan ">Salir</span></a>
 						</li>
 					<?php }else{ ?>
 
