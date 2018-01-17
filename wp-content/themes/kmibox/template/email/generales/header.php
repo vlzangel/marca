7abbc302acfd1ead7d98ad8c6a54268f458f85fc
kharlos <32462475+yookharlos@users.noreply.github.com>
2018-01-17 13:31:10 -0400

    observaciones

--- wp-content/themes/kmibox/template/email/generales/header.php
+++ wp-content/themes/kmibox/template/email/generales/header.php
@@ -5,7 +5,7 @@
 
 		<style type="text/css">
 			#body{
-				
+				width: 600px;
 				margin: 0px auto;
 				font-family: Arial; 
 				font-size: 14px; 
@@ -19,7 +19,15 @@
 				    line-height: 15px !important;
 				    padding: 5px !important;
 				}
-				
+				#body{
+				width: 919px;
+				margin: 0px auto;
+				font-family: Arial; 
+				font-size: 14px ! important; 
+				}
+				.text{
+					font-size: 14px ! important; 
+				}
 			}
 
 		</style>
