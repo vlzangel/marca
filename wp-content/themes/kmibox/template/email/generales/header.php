28b27ac77c7a4ee92cd7459d3f997ebca6922525
kharlos <32462475+yookharlos@users.noreply.github.com>
2018-01-17 13:26:23 -0400

    observaciones

--- wp-content/themes/kmibox/template/email/generales/header.php
+++ wp-content/themes/kmibox/template/email/generales/header.php
@@ -5,7 +5,7 @@
 
 		<style type="text/css">
 			#body{
-				width: 600px;
+				
 				margin: 0px auto;
 				font-family: Arial; 
 				font-size: 14px; 
@@ -19,6 +19,7 @@
 				    line-height: 15px !important;
 				    padding: 5px !important;
 				}
+				
 			}
 
 		</style>
