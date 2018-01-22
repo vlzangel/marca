213da6bc2823f6dc807ddee3068de9db3f6cff9d
Angel Veloz <vlzangel91@gmail.com>
2018-01-22 18:54:02 +0100

    Correos - Pago en tienda

---
+++ wp-content/themes/kmibox/template/email/compra/nuevo/tienda.php
@@ -0,0 +1,45 @@
+<div style="font-size: 25px; background: #0b1805; color: #FFF; text-align: center; padding: 30px 20px; font-weight: 600;">
+	¡Hola [USUARIO]!
+	<div  class="text" style="font-size: 17px; padding: 10px 20px 0px; font-weight: 400;"> 
+		Haz realizado un pago en tienda
+	</div>
+</div> 
+
+<div>
+	<img src="[IMG_PATH]pago_tarjeta/pago_tarjeta01.png" style="width: 100%; margin: 0px 0px 10px; border-bottom: solid 10px #75e417;" />
+</div>
+
+<br><br>
+
+<div  style="text-align: center;font-size: 16px; margin: 0px 0px 35px;">
+	Verifica <strong>los detalles de tu pago</strong>
+</div>
+
+<div style="text-align: center; font-size: 17px;">
+	<a href="[INSTRUCCIONES]" style="    
+		font-weight: 600;
+	    margin: 20px auto;
+	    width: 300px;
+	    border: solid 1px #CCC;
+	    border-radius: 5px;
+	    padding: 10px;
+	    background: #31a32e;
+	    color: #FFF;
+	    display: block;
+	    font-size: 13px;
+	    text-decoration: none;
+	    text-transform: uppercase;
+	    ">Instrucciones para el pago</a>
+</div>
+
+<br>
+
+<div style="text-align: center; font-size: 17px;">
+	<div class="mayuscula" style="font-weight: 600;">Total a pagar</div>
+	<div style="font-weight: 600;">$ [TOTAL] MXN</div>
+</div>
+
+<br><br>
+
+<div style="text-align: center; font-size: 17px;line-height: 27px;">¡Gracias por tu compra!</div>
+<br><br>
