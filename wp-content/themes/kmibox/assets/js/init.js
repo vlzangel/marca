6206ab24cadf30bb80bdc3b21655beaec96664b1
kharlos <32462475+yookharlos@users.noreply.github.com>
2018-01-23 09:45:26 -0400

    observaciones

--- wp-content/themes/kmibox/assets/js/init.js
+++ wp-content/themes/kmibox/assets/js/init.js
@@ -334,7 +334,114 @@ $(function($){
 	
 
  	
-	
+	// ***************************************
+	// Validar form pago
+	// ***************************************
+	$('#form-pago')
+	.on('success.form.bv', function(e) {
+	    e.preventDefault();
+	    jQuery("#btn_pagar_1").text("Procesando...");
+
+	    jQuery.post(
+			TEMA+"assets/ajax/suscribir_tarjeta.php",
+			jQuery(this).serialize(),
+			function(data){
+				console.log( data );
+				if( data["error"] == "" ){
+					jQuery("#pagar").addClass("hidden");
+					jQuery("#pago_exitoso").removeClass("hidden");
+					jQuery("#btn_pagar_1").text("Realizar Pago");
+				}else{
+					alert("Error, ver en la consola javascript");
+				}
+			}, "json"
+		).fail(function(e) {
+			console.log( e );
+	  	});
+
+	})
+	.bootstrapValidator({
+	    feedbackIcons: {
+	        valid: 'glyphicon glyphicon-ok',		        
+	        invalid: 'glyphicon glyphicon-remove',
+	        validating: 'glyphicon glyphicon-refresh'
+	    },
+		submitHandler: function(validator, form, submitButton){
+		    validator.defaultSubmit();
+		},    
+	    fields: {		    
+			num_card: {
+			    message: 'Error',
+			    validators: {
+			        notEmpty: {
+			            message: 'Este campo no debe estar vacío'
+			        },
+			        creditCard: {
+				        message: 'El número de tarjeta de credito no es valido'
+			        },
+			        stringLength: {
+			        max: 16,
+			        message: 'El número de tarjeta de credito no es valido'
+		      		}
+		    	}
+			},
+			cvv: {
+				message: 'Error',
+				validators: {
+					notEmpty: {
+						message: 'Este campo no debe estar vacío'
+					},
+					cvv: {
+						creditCardField: 'num_card',
+						message: 'El código de serguridad es invalido'
+					},
+				},
+			},
+			exp_month: {
+				message: 'Error',
+				validators: {
+					notEmpty: {
+						message: 'Este campo no debe estar vacío'
+					},
+					between: {
+	                    min: 01,
+	                    max: 12,
+	                    message: 'Debe seleccionar un mes valido'
+	                }
+				},
+			},
+			exp_year: {
+				message: 'Error',
+				validators: {
+					notEmpty: {
+						message: 'Este campo no debe estar vacío'
+					},
+					between: {
+	                    min: 01,
+	                    max: 99,
+	                    message: 'Debe seleccionar un año valido'
+	                }
+				},
+			},
+			holder_name: {
+				message: 'Error',
+				validators: {
+					notEmpty: {
+						message: 'Este campo no debe estar vacío'
+					},
+					stringLength: {
+						max: 150,
+						min: 2,
+						message: 'Longitud Invalida'
+					},
+					regexp: {
+	                    regexp: /^[A-Za-z\s]+$/i,
+	                    message: 'Caracteres invalidos '
+	                }          
+				}
+			},
+	    }
+	});
 
 
 	jQuery("#tienda").on("click", function(e){
@@ -424,6 +531,8 @@ $(function($){
 
 			r = $.parseJSON(r);
 
+			console.log( r );
+
 			jQuery(".btn-register_").attr("disabled", false);
 			jQuery(".btn-register_").html("Registrarme");
 
@@ -431,7 +540,7 @@ $(function($){
 				
 				jQuery("#success_registrando").css("display", "block");
 
-				setTimeout(function(){
+				/*setTimeout(function(){
 					var redirect = $('[name="redirect"]').val();
 					if( typeof $('[name="redirect"]').val() == 'undefined'){
 						redirect = '';
@@ -441,7 +550,7 @@ $(function($){
 					}else{
 						window.location.reload();				
 					}			
-				}, 1000);
+				}, 1000);*/
 
 			}else{
 				$('#login-mensaje').html(r['msg']);
@@ -839,7 +948,7 @@ $(function($){
 	    }
 	});
 
-	/*////////img del rgistro, cuando compras y no estas registrado/////////
+	/////////img del rgistro, cuando compras y no estas registrado/////////
 	$('inicio-sesion').on('click', '[href="#registro"]', function(){
 		var obj = $('#'+$(this).data('target'));
 		$('#imgclick').addClass('hidden');	});*/
@@ -864,7 +973,7 @@ $(function($){
 
 		$('#login-mensaje').html('');
 		$('#login-mensaje').addClass('hidden');
-		$.post( TEMA+"/procesos/login/recuperar.php", {
+		$.post( urlbase+"/ajax/reset_password.php", {
 			email: $('[name="email"]').val(),
 		}, function(r) {
 			var datos = $.parseJSON(r);
