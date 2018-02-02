1b7da5becdd4f4b2ce1c6372cde40fd7b3cc12a9
Italo <italococchini@gmail.com>
2018-02-01 20:07:31 -0400

    ajustes

--- wp-content/themes/kmibox/footer.php
+++ wp-content/themes/kmibox/footer.php
@@ -4,6 +4,7 @@
 		<script type="text/javascript" src="<?php echo get_home_url(); ?>/wp-content/themes/kmibox/lib/jquery.waterwheelCarousel.min.js"></script>
 		<script type='text/javascript' src="<?php echo get_home_url(); ?>/plugins/bootstrap-validator/bootstrapValidator.js"></script>
 		<script type='text/javascript' src="<?php echo get_home_url(); ?>/js/backpanel.js"></script>
+		<script type='text/javascript' src="<?php echo get_home_url(); ?>/js/cargar-orden.js"></script>
 		<script type='text/javascript' src="<?php echo get_home_url(); ?>/js/init.js"></script>
 
 		<?php 
@@ -11,26 +12,7 @@
 			if( $post->post_name == "quiero-mi-marca" ){
 			 	wp_footer(); 
 			}
-			// cargarTablaProductos();
-
-			if(  $_SESSION['admin_sub_login'] == 'YES' ){
-		        $HTML .= "
-		            <a href='".get_home_url()."/?i=".md5($_SESSION['id_admin'])."&admin=YES' class='theme_button' style='
-		                position: fixed;
-		                display: inline-block;
-		                left: 50px;
-		                bottom: 50px;
-		                padding: 20px;
-		                font-size: 48px;
-		                font-family: Roboto;
-		                z-index: 999999999999999999;
-		            '>
-		                X
-		            </a>
-		        ";
-
-		        echo comprimir($HTML);
-		    }
+			// cargarTablaProductos()
 		?>
 	</body>
 </html>
---
+++ wp-content/themes/kmibox/template/email/compra/pagos/orden_de_pago_cliente.php
@@ -0,0 +1,59 @@
+<div style="font-family: Arial;">
+	<div style="font-size: 25px; background: #0b1805; color: #FFF; text-align: center; padding: 30px 20px; font-weight: 600;">
+		¡Hola [USUARIO]!
+	</div> 
+
+	<div>
+		<img src="[IMG_PATH]confirmacion/header.png" style="width: 100%; margin: 0px 0px 10px; border-bottom: solid 10px #75e417;" />
+	</div>
+
+	<div style="text-align: center; font-size: 17px; line-height: 100%; margin: 30px 0px 40px;">
+		<strong style="color: #101705;">Tu solicitud de compra ya fue registrada.</strong> Para realizar el pago de tu compra debes acceder al siguiente enlace.
+	</div>
+
+	<div style="text-align: center; font-size: 15px;clear:both;padding:25px 0px;">
+		<a href="" style="text-decoration: none;color:#fff;padding:20px;border-radius:15px;background-color: #52ad05;font-weight: bold;">REALIZAR PAGO</a>
+	</div>
+
+	<div style="text-align: center; font-size: 15px;clear:both;">
+		<a style="text-decoration: none;" href="">Cancelar orden</a>
+	</div>
+
+	<div style="text-align: left; font-size: 15px;">
+		<div style="margin: 20px 0px 10px; font-weight: 600; text-transform: uppercase;">Direcci&oacute;n de env&iacute;o</div>
+		<div style="color: #535353; line-height: 120%;">
+			[DIRECCION]
+		</div>
+	</div>
+
+	<div style="text-align: left; font-size: 15px; line-height: 100%;">
+		<div style="margin: 20px 0px 10px; font-weight: 600; text-transform: uppercase;">Fecha de entrega estimada</div>
+		<label style="color: #535353;">[FECHA_ESTIMADA]</label>
+	</div>
+
+	<div style="text-align: left;font-size: 15px;line-height: 100%;">
+		<div style="margin: 20px 0px 10px; font-weight: 600; text-transform: uppercase;">tipo de env&iacute;o</div>
+		<label style="color: #535353;">Aeroflash</label>
+	</div>
+
+	<div style="text-align: left;font-size: 15px;line-height: 100%;">
+		<div style="margin: 20px 0px 10px; font-weight: 600; text-transform: uppercase;">Gu&iacute;a de rastreo</div>
+		<label style="color: #535353;">[GUIA]</label>
+	</div>
+
+	<div style="text-align: center; font-size: 14px; line-height: 100%; margin: 30px 0px; border-bottom: solid 2px #CCC; padding: 0px 3px 10px;">
+		<label style="color: #535353;"></label>
+	</div>
+
+	<div style="font-size: 15px;">
+		<table cellspacing=0 cellpadding=0 style="width: 100%;">
+			[PRODUCTOS]
+		</table>
+	</div>
+
+	<div style="text-align: center; font-size: 15px; line-height: 120%; margin: 30px 0px;">
+		<div style="margin: 20px 0px 10px; font-weight: 600; text-transform: uppercase; color: #101705; font-size: 17px;">Total Suscripci&oacute;n</div>
+		<div style="color: #101705; font-weight: 600; font-size: 24px; margin: 20px 0px;">$ [TOTAL] MXN</div>
+	</div>
+
+</div>
