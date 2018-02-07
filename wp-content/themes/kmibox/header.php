fa64ccc8c2ffe4e44bf236c31beec394eb0ea995
Angel Veloz <vlzangel91@gmail.com>
2018-02-07 13:11:17 +0100

    Ajuste favicon

--- wp-content/themes/kmibox/header.php
+++ wp-content/themes/kmibox/header.php
@@ -7,16 +7,16 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<link rel="shortcut icon" href="<?php echo TEMA(); ?>/imgs/favicon/favicon.ico" type="image/x-icon" />
-	<link rel="apple-touch-icon" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon.png" />
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon-57x57.png" />
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon-72x72.png" />
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon-76x76.png" />
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon-114x114.png" />
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon-120x120.png" />
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon-144x144.png" />
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon-152x152.png" />
-	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo TEMA(); ?>/imgs/favicon/apple-touch-icon-180x180.png" />
+	<link rel="shortcut icon" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/favicon.ico" type="image/x-icon" />
+	<link rel="apple-touch-icon" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon.png" />
+	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon-57x57.png" />
+	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon-72x72.png" />
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon-76x76.png" />
+	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon-114x114.png" />
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon-120x120.png" />
+	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon-144x144.png" />
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon-152x152.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo HOME(); ?>/wp-content/themes/kmibox//imgs/favicon/apple-touch-icon-180x180.png" />
 
 	<!-- BEGIN Style - Plugins -->
 	<link rel="stylesheet" href="<?php echo HOME(); ?>/plugins/bootstrap/css/bootstrap.css">
