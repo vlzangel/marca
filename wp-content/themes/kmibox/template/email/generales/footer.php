6f99f3eeee7a8172e498646339420f98a05ebaac
Italo <italococchini@gmail.com>
2018-02-23 15:38:57 -0400

    cambio de estilo footer email

--- wp-content/themes/kmibox/template/email/generales/footer.php
+++ wp-content/themes/kmibox/template/email/generales/footer.php
@@ -1,4 +1,4 @@
-			<div style="">
+			<div style="width: 100%">
 
 				<div style="    
 			    		text-align: center;
@@ -9,119 +9,52 @@
 					<strong>5540034824</strong> donde con gusto te atenderemos.
 				</div>
 
-				<!-- <table style="
-						position: relative;
-    					overflow: hidden;
-    					margin-top: 25px;">
-    				<tr>
+				<div style="
+					position: relative;
+					overflow: hidden;
+					margin-top: 25px;
+					">
 
-						<td style="width: 100%; text-align: right; padding: 0px 5px;">
-                            <img id="img_footer" src="[IMG_PATH]footer/footer_img.png" style="width: 100%;" />
-							<a href="https://kmimos.com.mx" style="padding:15px;border-radius:25px;background:#34cec0;">SOLICITAR CUPÓN</a>
-						</td>
-
-						<td id="recuerda" style="
-						        width: 100%;
-							    font-size: 16px;
-							    line-height: 30px;
-							    text-align: justify;
-							    border-top: solid 3px #CCC;
-							    border-bottom: solid 1px #CCC;
-							    padding: 20px;
-							    box-sizing: border-box;"
-						>
-							<img src="[IMG_PATH]footer/logo-kmimos-color.png" width="100px" /> es <strong>la mayor red de cuidadores certificados de M&eacute;xico</strong>, que cuidan a las mascotas en el hogar del cuidador, sin jaulas ni encierros.
-
-						</td>
-						<td>
-							<div style="height:20px;padding:0px!important;margin:0px!important;background-color: #000; border-top: 4px solid #a4d031"></div>
-						</td>
-
-					</tr>
-
-				</table> -->
-				<table style="
-						position: relative;
-    					overflow: hidden;
-    					margin-top: 25px;
-    					max-width: height: 387px;">
-    				<tr>
-						<td style="text-align: center;">
-							<img src="[IMG_PATH]footer/footer_img.png" style="width: 100%">
-							<br>
-							<br>
-							<a href="https://www.kmimos.com.mx">
-								<button style="    
-									padding: 10px 15px;
-								    border-radius: 50px;
-								    border: 0px; 
-								    background: #34cec0;
-								    color: #fff!important;
-								    cursor: pointer;
-								    font-size: 25px;
-								    font-weight: bold;
-								    font-family: 'Gotham';
-								    text-decoration: none;
-								    ">SOLICITAR CUPÓN</button>
-							</a>
-						</td>
-					</tr>
-    				<tr>
-						<td id="recuerda" style="
-						        width: 100%;
-							    font-size: 17.5px;
-							    line-height: 30px;
-							    text-align: justify;
-							    
-							    border-bottom: dashed 3px #CCC;
-							    padding: 20px;
+						<img src="[IMG_PATH]footer/footer_img.png" style="width: 100%">
+						<br>
+						<br>
+						<a href="https://www.kmimos.com.mx">
+							<button style="    
+								padding: 10px 15px;
+							    border-radius: 50px;
+							    border: 0px; 
+							    background: #34cec0;
+							    color: #fff!important;
+							    cursor: pointer;
+							    font-size: 25px;
+							    font-weight: bold;
 							    font-family: 'Gotham';
-							    line-height: 1.4;
-							    box-sizing: border-box;"
-						>
-							<img src="[IMG_PATH]footer/logo-kmimos-color.png" width="100px" /> <strong>es la mayor red de cuidadores certificados de M&eacute;xico</strong>, <span style="font-family: gotham-light;font-weight: lighter;">que cuidan a las mascotas en el hogar del cuidador, totalmente libres de jaulas ni encierros, con mas de 1000 cuidadores registrados.
-						</td>
-					</tr>
-    				<tr>
-						<td>
-							<div style="height:35px;padding:0px!important;margin:10px 0px!important;background-color: #000; border-top: 5px solid #a4d031"></div>
-						</td>
-					</tr>
-				</table>
+							    text-decoration: none;
+							    ">SOLICITAR CUPÓN</button>
+						</a>
+				</div> 
+				<div id="recuerda" style="
+				        width: 100%;
+					    font-size: 18.5px;
+					    line-height: 30px;
+					    text-align: justify;
+					    
+					    border-bottom: dashed 3px #CCC;
+					    padding: 20px;
+					    font-family: 'Gotham';
+					    line-height: 1.4;
+					    box-sizing: border-box;"
+				>
+					<img src="[IMG_PATH]footer/logo-kmimos-color.png" width="100px" /> <strong>es la mayor red de cuidadores certificados de M&eacute;xico</strong>, <span style="font-family: gotham-light;font-weight: lighter;">que cuidan a las mascotas en el hogar del cuidador, totalmente libres de jaulas ni encierros, con mas de 1000 cuidadores registrados.
+				</div>
+				<div style="height:35px;padding:0px!important;margin:10px 0px!important;background-color: #000; border-top: 5px solid #a4d031"></div>
+				
 
 
 
 			</div>
 
-			<!-- <div style="background: #0b1805; color: #FFF; margin-top: 25px;">
-				
-				<table style="">
-    				<tr>
-
-						<td style="width: 60%; text-align: left; padding: 25px;">
-							<a href="https://www.facebook.com/">
-								<img class="iconos" src="[IMG_PATH]footer/fb.png" style="width: 14%; float: left; margin: 0px 3px;" />
-							</a>
-							<a href="https://twitter.com/">
-								<img class="iconos" src="[IMG_PATH]footer/tw.png" style="width: 14%; float: left; margin: 0px 3px;" />
-							</a>
-							<a href="https://www.instagram.com/">
-								<img class="iconos" src="[IMG_PATH]footer/ig.png" style="width: 14%; float: left; margin: 0px 3px;" />
-							</a>
-							<a href="https://www.youtube.com/">
-								<img class="iconos" src="[IMG_PATH]footer/yt.png" style="width: 14%; float: left; margin: 0px 3px;" />
-							</a>
-						</td>
-
-						<td style="width: 40%; text-align: right; padding: 10px 25px;" >
-							<img src="[IMG_PATH]footer/logo_new.png" style="width: 100%;" />
-						</td>
-
-					</tr>
-
-				</table>
-
-			</div> -->
+			 
 
 		</div>
 
