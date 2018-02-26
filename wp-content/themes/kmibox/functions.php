aa87335d2dd698727a5f099aab1d898843df700e
Angel Veloz <vlzangel91@gmail.com>
2018-02-26 00:49:17 -0430

    Ajustes: QUIERO CONVERTIRME EN ASESOR

--- wp-content/themes/kmibox/functions.php
+++ wp-content/themes/kmibox/functions.php
@@ -59,7 +59,7 @@
 		remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
 		// Remove oEmbed-specific JavaScript from the front-end and back-end.
 		remove_action( 'wp_head', 'wp_oembed_add_host_js' );
-		add_filter( 'tiny_mce_plugins', 'disable_embeds_tiny_mce_plugin' );
+		//add_filter( 'tiny_mce_plugins', 'disable_embeds_tiny_mce_plugin' );
 		// Remove all embeds rewrite rules.
 		// add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
 		// Remove filter of the oEmbed result before any HTTP requests are made.
