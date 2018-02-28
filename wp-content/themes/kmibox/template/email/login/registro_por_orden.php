3cf9cc52076ed5184718d0c90e3f5492cb1c0a3d
Angel Veloz <vlzangel91@gmail.com>
2018-02-28 17:34:22 +0100

    Act

--- wp-content/themes/kmibox/template/email/login/registro_por_orden.php
+++ wp-content/themes/kmibox/template/email/login/registro_por_orden.php
@@ -90,5 +90,5 @@
 	margin: 30px 0px;
 	display: block;
 ">
-	Si recibiste este correo sin haberte registrado, por favor ign&oacute;ralo.
+	Si recibiste este correo sin haberte registrado, por favor contáctanos.
 </label> 
