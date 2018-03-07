5672ee8f9efa9f6c4f961a8ab3e3e2b07a0d82bb
Angel Veloz <vlzangel91@gmail.com>
2018-03-07 19:15:47 -0430

    Ajustes registro
    Registro - El númro de teléfono fijo no debe ser obligatorio.
    Registro - El código del asesor y el correo del asesor no deben ser obligatorios.
    Registro - Si un cliente se registra sin asesor, se debe enviar un correo especial a los administradores indicándoselos.

---
+++ wp-content/themes/kmibox/template/email/login/sin_asesor.php
@@ -0,0 +1,28 @@
+<div style="
+	font-size: 25px; 
+	background: #0b1805; 
+	color: #FFF; 
+	text-align: center; 
+	padding: 30px 20px; 
+	font-weight: 600;
+">
+	¡Hola <span style="text-transform: capitalize;">Administrador</span>!
+	<div   class="text" style="font-size: 17px; padding: 10px 20px 0px; font-weight: 400;"> 
+		Se ha realizado un registro sin un asesor asociado
+	</div>
+</div> 
+
+<div class="text" style="
+	text-align: center;
+	font-size: 17px;
+">
+	<div style="padding: 40px 0px 0px;">
+		Te no tificamos que el usuario <strong>[USUARIO]</strong> ha realizado un registro sin un c&oacute;digo de asesor con el email
+		<a href="mailto:[EMAIL]" target="_blank" style="
+			font-weight: 400;
+		    color: #000;
+		    text-decoration: none;
+		">[EMAIL]</a>.
+	</div>
+
+</div>
