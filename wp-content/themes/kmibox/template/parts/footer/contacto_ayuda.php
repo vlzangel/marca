f8d2651f3288faa0c9a96036be0bda767ce5ca75
Angel Veloz <vlzangel91@gmail.com>
2018-03-13 20:41:35 -0430

    Act

--- wp-content/themes/kmibox/template/parts/footer/contacto_ayuda.php
+++ wp-content/themes/kmibox/template/parts/footer/contacto_ayuda.php
@@ -2,10 +2,10 @@
 <section id="contacto-ayuda" >
 
 	
-	<img class="solo_pc contacto-ayuda-izquierda" src="<?php echo TEMA() . '/imgs/popup-nutriheroes/footer/izq.png'; ?>">
+	<img class="solo_pc contacto-ayuda-izquierda" src="<?php echo TEMA() . '/imgs/popup-nutriheroes/footer/izq_2.png'; ?>">
 	<img class="solo_pc contacto-ayuda-derecha" src="<?php echo TEMA() . '/imgs/popup-nutriheroes/footer/der.png'; ?>">
 	
-	<img class="solo_movil contacto-ayuda-izquierda" src="<?php echo TEMA() . '/imgs/popup-nutriheroes/footer/izq-responsive.png'; ?>">
+	<img class="solo_movil contacto-ayuda-izquierda" src="<?php echo TEMA() . '/imgs/popup-nutriheroes/footer/izq_2-responsive.png'; ?>">
 	<img class="solo_movil contacto-ayuda-derecha" src="<?php echo TEMA() . '/imgs/popup-nutriheroes/footer/der-responsive.png'; ?>">
 
 </section>
