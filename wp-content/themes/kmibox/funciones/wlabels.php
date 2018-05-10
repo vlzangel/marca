3d95d7564cd98ca420d557008c49f9faa82f11c9
Angel Veloz <vlzangel91@gmail.com>
2018-05-10 12:42:05 +0200

    Ajuste wLabel

--- wp-content/themes/kmibox/funciones/wlabels.php
+++ wp-content/themes/kmibox/funciones/wlabels.php
@@ -1,10 +1,44 @@
 <?php
 	
+	function getAllWlabel(){
+		return array(
+			"momsweb" => array(
+				"wl" => "momsweb",
+				"codigo" => "1000001",
+				"asesor" => "momsweb.mx",
+				"asesor_email" => "momsweb.mx@mail.com",
+			),
+			"alsea" => array(
+				"wl" => "alsea",
+				"codigo" => "1000002",
+				"asesor" => "alsea.mx",
+				"asesor_email" => "alsea.mx@mail.com",
+			),
+			"jj" => array(
+				"wl" => "jj",
+				"codigo" => "1000003",
+				"asesor" => "jj.mx",
+				"asesor_email" => "jj.mx@mail.com",
+			),
+			"quitar" => array(),
+		);
+	}
+
+	function is_wlabel($email){
+		if($email == ""){ return false; }
+		$wlabels = getAllWlabel();
+		foreach ($wlabels as $key => $wlabel) {
+			if( $wlabel["asesor_email"] == $email ){
+				return $key;
+			}
+		}
+		return false;
+	}
+	
 	function wlabel(){
 		if( !isset($_SESSION) ){ session_start(); }
 		if( isset( $_SESSION["wlabel"] ) ){
 			echo '<link rel="stylesheet" href="'.TEMA().'wlabels/css/'.$_SESSION["wlabel"]["wl"].'.css?v='.time().'">';
 		}
-
 	}
 ?>
