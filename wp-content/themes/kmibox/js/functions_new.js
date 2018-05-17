279b00832a3b1e447867c6ef51b75b9b7444848b
Ángel Veloz <vlzangel91@gmail.com>
2018-05-17 16:55:32 -0430

    Ajustes trello

--- wp-content/themes/kmibox/js/functions_new.js
+++ wp-content/themes/kmibox/js/functions_new.js
@@ -518,6 +518,7 @@ function loadPresentaciones(){
 									'<div class="peso_producto_box">'+producto.peso+'</div>'+
 									'<div class="existencia_producto_box">'+existencia+'</div>'+
 									'<div class="title_producto_box">'+FN(producto.precio)+" MXN"+'</div>'+
+									'<div class="comprar_producto_box"> <div class="">COMPRAR</div> </div>'+
 								'</div>'+
 							'</div>'+
 						'</div>';
