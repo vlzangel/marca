20ff9e5db3ac0a3ebe2531a1844b673da7c371a8
Italo Cocchini <italococchini@gmail.com>
2018-05-31 17:12:31 -0400

    popup royal canin

--- wp-content/themes/kmibox/template/parts/popup/royalcanin/royalcanin.php
+++ wp-content/themes/kmibox/template/parts/popup/royalcanin/royalcanin.php
@@ -2,15 +2,13 @@
 <div class="backdrop-popup" id="royalcanin" style="display:none;">
 
 		<div class="col-sm-12 col-md-6 col-md-offset-3">
-			<div class="text-right" style="position: relative;">
-				<a href="<?php echo get_home_url(); ?>/quiero-mi-marca/<?php echo get_source_url(); ?>">
-					<i class="fa fa-close" id="popup-cerrar"></i>
-				</a>	
-				
-				<a href="<?php echo get_home_url(); ?>/quiero-mi-marca/<?php echo get_source_url(); ?>">
-					<img src="<?php echo TEMA(); ?>/imgs/popup-royalcanin/popup-royalcanin.png" class="img-responsive" style="width:90%;">
-				</a>	
-			</div>
+			<a href="<?php echo get_home_url(); ?>/quiero-mi-marca/<?php echo get_source_url(); ?>">
+				<i class="fa fa-close" id="popup-cerrar"></i>
+			</a>	
+			
+			<a href="<?php echo get_home_url(); ?>/quiero-mi-marca/<?php echo get_source_url(); ?>">
+				<img src="<?php echo TEMA(); ?>/imgs/popup-royalcanin/popup-royalcanin.png" class="img-responsive" style="width:90%;">
+			</a>	
 		</div>
 
 </div>
