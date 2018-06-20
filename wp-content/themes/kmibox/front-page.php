5bee17d242097d56c5868b3f0ae1b1e5772ce03d
Italo <italococchini@gmail.com>
2018-06-20 19:49:06 -0400

    Cambio solo wlabel momsweb:  sustitucion de la palabra "Asesor" por "Promotor"

--- wp-content/themes/kmibox/front-page.php
+++ wp-content/themes/kmibox/front-page.php
@@ -47,7 +47,7 @@ include_once( 'template/parts/footer/contacto_ayuda.php' );
 /* END POPUP Nutriheroes   */		
 	
 
-include_once( 'wlabels/parametros/momsweb.php' );
+parametros_wlabel();
 
 ?>
 	<?php echo MENU(); ?>
